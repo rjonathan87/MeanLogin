3b7c6e6254292e2c9deef48beaa9af129ca6346b
rjonathan87 <rjonathan87@gmail.com>
2019-11-05 13:04:48 -0600

    Auth Rounting

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,13 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+
+import { IUser } from '../models/IUser';
 import { IJwtResponse } from '../models/IJwtResponse';
+
 import { tap } from 'rxjs/operators';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
-}
+  AUTH_SERVER = 'http://localhost:3000';
+  authSubject = new BehaviorSubject(false);
+  private token: string;
 
-//https://www.youtube.com/watch?v=BCygvtZwkh4 min 14
+  constructor(private httpClient: HttpClient) { }
+
+  register(user: IUser): Observable<IJwtResponse> {
+    return this.httpClient.post<IJwtResponse>(`${this.AUTH_SERVER}/register`, user)
+      .pipe(tap(
+        (res: IJwtResponse) => {
+          if (res) {
+            // guardar token
+          }
+        }
+      ));
+  }
+  login(user: IUser): Observable<IJwtResponse> {
+    return this.httpClient.post<IJwtResponse>(`${this.AUTH_SERVER}/login`, user)
+      .pipe(tap(
+        (res: IJwtResponse) => {
+          if (res) {
+            // guardar token
+          }
+        }
+      ));
+  }
+  logout(): void {
+    this.token = '';
+    localStorage.removeItem('ACCESS_TOKEN');
+    localStorage.removeItem('EXPIRES_IN');
+  }
+  private saveToken(token: string, expiresIn: string): void{
+    localStorage.setItem('ACCESS_TOKEN', token);
+    localStorage.setItem('EXPIRES_IN', expiresIn);
+  }
+}
